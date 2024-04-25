c4c131200d46801d854821ff0930cb6e2edfd01d
Alberto Espinosa <110586852+albertoysaac@users.noreply.github.com>
2024-04-25 05:28:57 +0000

    api basica terminada

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,45 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup():
+    request_body = request.get_json()
+    if User.query.filter_by(email=request_body["email"]).first():
+        return jsonify({"msg": "User already exists"}), 400
+    else:
+        user = User(names=request_body["name"], last_name=request_body["last_name"], age = request_body["age"],
+                email=request_body["email"], password=request_body["password"], is_active=True)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"msg": "User created"}), 200
+    
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    user = User.query.filter_by(email=email).first()
+    
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad username or password"}), 401
 
-    return jsonify(response_body), 200
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route('/users', methods=['GET'])
+@jwt_required()
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
