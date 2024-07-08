f4df9a83c172ddcf0c99ceb06cf37fce0769eaa3
Alberto Espinosa <110586852+albertoysaac@users.noreply.github.com>
2024-07-08 01:00:41 +0000

    ejercicio terminado

--- src/api/models.py
+++ src/api/models.py
@@ -4,18 +4,18 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    names = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
-    age = db.Column(db.Integer, unique=False, nullable=False)
+    names = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
+            "id": self.id,
             "names": self.names,
             "last_name": self.last_name,
             "age": self.age,
